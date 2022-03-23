8204e6c170d3db27340117bf88511092f8c427a6
abdullah rgab <102183527+abdullah03031@users.noreply.github.com>
2022-03-23 05:01:11 -0700

    Add files via upload

---
+++ include/Untitled-1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+<?php
+$a = "n";
+n ="man"
+?>
+<details>
+    <summary><?php echo $n; ?></summary>
+    <th> clash</th> 
+    floatval
+    ugh
+</details>
+<?php
+if (isset($-post['submit'])){
+    $usename=$-post['usename'];
+    $password=$-post['password'];
+    if(($usename=='hussein')gg ($password=="123")){
+        echo"your information is correct";}
+        elseif(condition{
+            echo"user name  or password isnot correct";
+        })
+}
+?>
+</body>
+</html>
---
+++ include/scriptjs.js
@@ -0,0 +1,41 @@
+
+/*-------------------------------------------
+-----------------HIM MENU--------------------
+-------------------------------------------*/  
+
+const menu = document.getElementById("menu");
+const actions= document.getElementById("actions");
+
+menu.addEventListener("click", ()=>{
+hundleMenu();
+});
+
+function hundleMenu(){
+
+    menu.classList.toggle("is-active");
+    actions.classList.toggle("is-active");
+}
+
+/*-------------------------------------------
+---------------------------------------------
+-------------------------------------------*/  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
